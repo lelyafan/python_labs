cd2c34d0b3c9b0c844ecbf8753d74d1c6d7ac9c5
lelyafan <lazarevaelena2107@gmail.com>
2025-10-10 15:52:42 +0300

    Добавила

--- src/lab02/tuples.py
+++ src/lab02/tuples.py
@@ -1,13 +1,27 @@
 def format_record(rec: tuple[str, str, float]) -> str:
-    fio, gr, gpa = rec
-    fio2 = ' '.join(fio.split()).split()
-    fam = fio2[0]
+    try:
+        fio, gr, gpa = rec
+        fio2 = ' '.join(fio.split()).split()
+        fam = fio2[0]
 
-    ini = []
-    for i in range(1, len(fio2)):
-        if i <= 2:
-            ini.append(f'{fio2[i][0]}.')
-    res = f'{fam} {' '.join(ini)}, гр. {gr}, GPA {gpa:.2f}'
-    return res
-t = ("Иванов Иван Иванович", "BIVT-25", 4.6)
-print(format_record(t))
+        if (fio not in rec) or (gr not in rec) or (gpa not in rec):
+            return 'ValueError'
+
+        ini = []
+        for i in range(1, len(fio2)):
+            if i <= 2:
+                ini.append(f'{fio2[i][0]}.')
+        res = f'{fam} {' '.join(ini)}, гр. {gr}, GPA {gpa:.2f}'
+        return res
+    except:
+        return 'ValueError'
+a = ("Иванов Иван Иванович", "BIVT-25", 4.6)
+b = ("Петров Пётр", "IKBO-12", 5.0)
+c = ("Петров Пётр Петрович", "IKBO-12", 5.0)
+d = ("  сидорова  анна   сергеевна ", "ABB-01", 3.999)
+f = ("СС", "", "")
+print(format_record(a))
+print(format_record(b))
+print(format_record(c))
+print(format_record(d))
+print(format_record(f))
